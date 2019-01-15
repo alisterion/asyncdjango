b86a43eee98327a974d702fb417f6b0c44acb4c6
Oleksandr Rehush <o.rehush@bvblogic.com>
2019-01-15 19:12:26 +0200

    models structure

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 Taxi app (Prototype of async app in django)
 --
 
-## Set up
+### Set up
 
 0. install python3.5 virtualenv rabbitmq-server python3-dev
 1. mkvirtualenv -p /usr/bin/python3.5 asyncdjango
-2. pip install django djangorestframework channels celery
+2. pip install django djangorestframework channels celery django-extensions django-ordered-model
 3. django-admin startproject asyncdjango
 4. cd asyncdjango/asyncdjango/
 5. django-admin startapp app
 6. follow instructions:
->   https://www.django-rest-framework.org/
->   https://channels.readthedocs.io/en/latest/installation.html
->   http://docs.celeryproject.org/en/latest/django/first-steps-with-django.html
+    * https://www.django-rest-framework.org/
+    * https://channels.readthedocs.io/en/latest/installation.html
+    * http://docs.celeryproject.org/en/latest/django/first-steps-with-django.html
--- asyncdjango/app/models.py
+++ asyncdjango/app/models.py
@@ -1,26 +1,58 @@
 from django.conf import settings
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
+from ordered_model.models import OrderedModel
 
 from asyncdjango.app.choices import OrderStatus, OrderEventStatus
 
 
-class Queue(models.Model):
+class Queue(OrderedModel):
     driver = models.OneToOneField(settings.AUTH_USER_MODEL, models.CASCADE)
-    number = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return '{}: {}'.format(self.driver, self.number)
+        return '{}: {}'.format(self.driver, self.order)
 
 
-class Order(models.Model):
-    client = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE, related_name='my_orders')
-    driver = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE, related_name='orders', null=True, blank=True)
+class Order(TimeStampedModel):
+    client = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        models.CASCADE,
+        related_name='my_orders'
+    )
+    driver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        models.CASCADE,
+        related_name='orders',
+        null=True,
+        blank=True
+    )
     description = models.TextField(max_length=255)
-    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
     status = models.PositiveIntegerField(choices=OrderStatus.choices())
 
+    def __str__(self):
+        return 'Order {}: {}'.format(self.id, self.client)
 
-class OrderEvent(models.Model):
-    order = models.ForeignKey(Order, models.CASCADE)
-    driver = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE)
-    status = models.PositiveIntegerField(choices=OrderEventStatus.choices())
+
+class OrderEvent(TimeStampedModel):
+    order = models.ForeignKey(
+        Order,
+        models.CASCADE,
+        related_name='events'
+    )
+    driver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        models.CASCADE,
+        related_name='events'
+    )
+    status = models.PositiveIntegerField(
+        choices=OrderEventStatus.choices()
+    )
+
+    def __str__(self):
+        return 'Order event {}: {}'.format(self.id, self.driver)
